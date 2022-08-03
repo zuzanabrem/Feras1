c18661b7c4a84d09b706ae36e1ed93ccd67e09cb
zuzanabrem <85314636+zuzanabrem@users.noreply.github.com>
2022-08-03 19:12:54 +0200

    add file

---
+++ main.js
@@ -0,0 +1,12 @@
+let navbar = document.querySelector('.navbar')
+
+document.querySelector('#menu-bar').onclick = () =>{
+    navbar.classList.toggle('active');
+}
+
+document.querySelector('#close').onclick = () =>{
+    navbar.classList.remove('active');
+}
+
+
+
